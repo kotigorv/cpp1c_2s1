869d7b4ab77b55a61c0c58f919233beef1234a48
2410509 <2410509@L10-11>
2025-01-29 11:57:02 +0200

    Add project files.

---
+++ lab1_3/include/lab1_3.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <iostream>
+#include <cmath>
+
+namespace lab1_3 {
+	
+	class Expression 
+	{
+	private: 
+		double a;
+		double b;
+	public: 
+		const double getA() const {
+			return this->a;
+		}
+
+		void setA(const double& a) {
+			this->a = a;
+		}
+
+		const double getB() const {
+			return this->b;
+		}
+
+		void setB(const double& b) {
+			this->b = b;
+		}
+
+		const double calculate(const double& x) const;                                                                                  
+	};
+}
+
---
+++ lab1_3/src/lab1_3.cpp
@@ -0,0 +1,30 @@
+﻿#include "lab1_3.hpp"
+
+using std::cout;
+using std::endl;
+using std::sin;
+using std::exp;
+using std::pow;
+using lab1_3::Expression;
+
+namespace lab1_3 {
+	const double Expression::calculate(const double& x) const {
+		auto r = 2 - pow(b - x, 1.0 / 5);
+
+		return sin(a * x) / (b - x) * exp(r);
+	}
+}
+
+int main()
+{
+	double a = 16.01, b = 2.19, x = -8.23;
+
+	Expression exp;
+
+	exp.setA(a);
+	exp.setB(b);
+
+	cout << exp.calculate(x) << endl;
+
+	return 0;
+}
